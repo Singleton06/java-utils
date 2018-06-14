103e0f98b6e870f2950b29e6121a2cab091ff9e9
bklager <40216838+bklager@users.noreply.github.com>
2018-06-14 15:34:32 -0500

    GH-1: Adding repository handling to code (#8)
    GH-1: Adding repository handling to code

--- src/main/pom-parser/pom-parser.js
+++ src/main/pom-parser/pom-parser.js
@@ -1,15 +1,60 @@
 const xml2js = require('xml2js');
 
 const getCoordinatesFromNode = (node) => {
-  const groupId = node.groupId ? node.groupId[0] : '';
-  const artifactId = node.artifactId ? node.artifactId[0] : '';
-  const version = node.version ? node.version[0] : '';
+  //parent checks work on the same level as the node so it is not possible for parents or dependencies 
+  const groupId = node.groupId ? node.groupId[0] : (node.parent ? getCoordinatesFromNode(node.parent[0]).groupId : undefined );
+  const artifactId = node.artifactId ? node.artifactId[0] : undefined;
+  const version = node.version ? node.version[0] : (node.parent ? getCoordinatesFromNode(node.parent[0]).version : undefined );
 
   return {
     groupId,
     artifactId,
     version
   };
+};
+
+const getRepositoryPolicy = (release) => {
+  const enabled = release.enabled ? release.enabled[0] : undefined;
+  const updatePolicy = release.updatePolicy ? release.updatePolicy[0] : undefined;
+  const checksumPolicy = release.checksumPolicy ? release.checksumPolicy[0] : undefined;
+
+  return {
+    enabled,
+    updatePolicy,
+    checksumPolicy
+  };
+};
+
+const readRepositoriesFromProject = (project) => {
+  if (!project.repositories || !project.repositories[0] || !project.repositories[0].repository) {
+    return [];
+  }
+
+  return project.repositories[0].repository.map((repository) => {
+    const id = repository.id ? repository.id[0] : undefined;
+    const url = repository.url ? repository.url[0] : undefined; 
+    const name = repository.name ? repository.name[0] : undefined;
+    const layout = repository.layout ? repository.layout[0] : undefined;
+
+    let releases = undefined;
+    let snapshots = undefined;
+
+    if(repository.releases && repository.releases[0]){
+      releases = getRepositoryPolicy(repository.releases[0]);
+    }
+    if(repository.snapshots && repository.snapshots[0]){
+      snapshots = getRepositoryPolicy(repository.snapshots[0]);
+    }
+
+    return{
+      id,
+      url,
+      name,
+      layout,
+      releases,
+      snapshots
+    };
+  });
 };
 
 const readDependenciesFromProject = (project) => {
@@ -36,7 +81,8 @@ const buildJSONStructure = (project) => {
   return {
     ...getCoordinatesFromNode(project),
     parent: parentCoordinates,
-    dependencies: readDependenciesFromProject(project)
+    dependencies: readDependenciesFromProject(project),
+    repositories: readRepositoriesFromProject(project)
   };
 };
 
